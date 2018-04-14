06be31216e6dd9a1c1c7dcc93769658d1c4e3f8b
Tom BERRIOT <tom.berriot@etudiant.univ-reims.fr>
2018-04-14 15:29:16 +0200

    Commit

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-var Discord = require('discord.io');
+require('dotenv').config()
+
+const Discord = require('discord.js')
+const tumblr = require('tumblr.js')
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom;
+const bot = new Discord.Client()
 var logger = require('winston');
-var auth = require('./auth.json');
+
+var PREFIX = '??'
+
+try {
+    var tumblrClient = new tumblr.createClient({
+        consumer_key: process.env.TUMBLR_API_KEY,
+        consumer_secret: process.env.TUMBLR_SECRET_KEY,
+        token: process.env.TUMBLR_TOKEN,
+        token_secret: process.env.TUMBLR_TOKEN_SECRET,
+        returnPromises: true,
+    });
+}
+catch(error){
+        logger.error(error)
+}
 
 
 // Configure logger settings
@@ -10,33 +30,80 @@ logger.add(logger.transports.Console, {
 });
 
 logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-    token: process.env.token,
-    autorun: true
-});
-bot.on('ready', function (evt) {
+
+
+bot.on('ready', function () {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == 'pedo!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+    logger.info(bot.user.username + ' - (' + bot.user.id + ')');
+    bot.user.setActivity("Type " + PREFIX + "help")
+})
 
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                break;
-            // Just add any case commands if you want to..
+bot.on('message', async message => {
+    try{
+        if (message.content.substring(0, PREFIX.length) === PREFIX) {
+            var args = message.content.substring(PREFIX.length).split(' ');
+            var cmd = args[0];
+            switch(cmd) {
+                case 'ping':
+                    message.reply('Pong!');
+                    break;
+                case 'setPrefix':
+                    if(args[1] === undefined) message.channel.send('You must provide a value for this command')
+                    else {
+                        PREFIX = args[1]
+                        bot.user.setActivity("Type " + PREFIX + "help")
+                        message.channel.send('The Command Prefix has been changed to : **' + PREFIX + '**')
+                    }
+                    break;
+                case 'd':
+                case 'devMemes':
+                    tumblrClient.blogInfo('thecodinglove.com')
+                        .then(data=>{
+                            var offset = Math.floor(Math.random() * data.blog.total_posts)
+                            return tumblrClient.blogPosts('thecodinglove.com', {offset: offset, limit: 1})
+                        })
+                        .then(data=>{
+                            var post = data.posts[0]
+                            const dom = new JSDOM(post.body)
+                            message.channel.send(
+                                '```asciidoc'+
+                                '\n# ' + post.summary+
+                                '```\n'+
+                                dom.window.document.querySelector('img').src
+                            )
+                        })
+
+                    break;
+                case 'lolis':
+                    message.channel.send("THEY ARE THE BEST")
+                    break;
+                case 'help':
+                    message.channel.send(
+                        '```Markdown' +
+                        '\n# Actual Prefix : ' + PREFIX+
+                        '\n' +
+                        '\n# Commands : ' +
+                        '\n   - ping : Pong !' +
+                        '\n   - setPrefix [value] : Change the prefix used before commands' +
+                        '\n   - devMemes (shortcut : d) : random dev memes across the web ' +
+                        '\n   - help'+
+                        '```'
+                    )
+                    break;
+
+            }
         }
+    }catch(error){
+        logger.error(error)
     }
+
 });
+
+
+try{
+    bot.login(process.env.DISCORD_TOKEN)
+}
+catch(error){
+    logger.error(error)
+}
