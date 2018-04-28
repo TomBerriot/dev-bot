86557c9a6f6a5c12cbb463a6b20114d78e3e7a1d
Tom BERRIOT <tom.berriot@etudiant.univ-reims.fr>
2018-04-28 22:16:55 +0200

    *

--- SourceManager/abs/SourceFactory.js
+++ SourceManager/abs/SourceFactory.js
@@ -1,8 +1,5 @@
 const SourceFactory = function SourceFactory() {
 };
 
-SourceFactory.prototype.getRandomMeme = function getRandomMeme() {
-	throw new Error('You must implement the getRandomMeme function');
-};
 
 module.exports = SourceFactory;
